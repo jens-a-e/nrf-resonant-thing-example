f7702220b62f97f6ebd54b0665118e243da4437b
jens <jens@lea.io>
2021-08-20 12:21:03 +0200

    Using more common logging crates + cargo-embed

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 #![no_main]
 #![no_std]
 
-use defmt_rtt as _; // global logger
-use nrf52840_hal as _; // memory layout
+use panic_probe as _;
+use rtt_target::{rprintln, rtt_init_print};
+// use nrf52840_hal as _; // memory layout
+
+// use nrf52840_hal::pac::Peripherals;
+// use nrf52840_hal::Temp;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    defmt::info!("Hello, World!");
-    exit();
+    rtt_init_print!();
+    rprintln!("Hello, World!");
+    
+    // let periph = Peripherals::take().unwrap();
+    // let mut temp_sensor = Temp::new(periph.TEMP);
+
+    // let temp: i32 = temp_sensor.measure().to_num();
+
+    // defmt::info!("cpu temp is {:}ºC", temp);
+    
+    brk();
 }
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    defmt::error!("panicked");
-    exit()
-}
+// #[panic_handler]
+// fn panic(_info: &core::panic::PanicInfo) -> ! {
+//     defmt::error!("panicked");
+//     exit()
+// }
 
-pub fn exit() -> ! {
+pub fn brk() -> ! {
     loop {
         cortex_m::asm::bkpt();
     }
